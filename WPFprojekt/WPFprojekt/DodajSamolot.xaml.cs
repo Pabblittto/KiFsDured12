243adf6715f2aa3d2fee8e4594787127d89a983f
Hubert Parzych <HubertParzych98@gmail.com>
2018-06-10 23:31:34 +0200

    Commit suicide

--- WPFprojekt/WPFprojekt/DodajSamolot.xaml.cs
+++ WPFprojekt/WPFprojekt/DodajSamolot.xaml.cs
@@ -19,20 +19,21 @@ namespace WPFprojekt
     /// </summary>
     public partial class DodajSamolot : Window
     {
-        public List<TypSamolotu> listaTypowSamolotow = new List<TypSamolotu>();
+        public List<TypSamolotu> listaTest = new List<TypSamolotu>();
         Firma tmp;
 
         public DodajSamolot(Firma Obiekt)
         {
-            listaTypowSamolotow.Add(new TypSamolotu("Ajirbuc", 100, 100, 100, 100));
-            listaTypowSamolotow.Add(new TypSamolotu("Bojeng", 101, 101, 101, 101));
-            listaTypowSamolotow.Add(new TypSamolotu("Tuplowew", 101, 101, 101, 101));
+            InitializeComponent();
+            listaTest.Add(new TypSamolotu("Ajirbuc", 100, 100, 100, 100));
+            listaTest.Add(new TypSamolotu("Bojeng", 101, 101, 101, 101));
+            listaTest.Add(new TypSamolotu("Tuplowew", 101, 101, 101, 101));
             initBind();
             tmp = Obiekt;// żeby te okienko widziało główną firmę
         }
         private void initBind()
         {
-            lista_TypSamolotu.ItemsSource = listaTypowSamolotow;
+            lista_TypSamolotu.ItemsSource = listaTest;
             CollectionView Lista1 = (CollectionView)CollectionViewSource.GetDefaultView(lista_TypSamolotu.ItemsSource);
             Lista1.Filter = NameFilter;
         }
